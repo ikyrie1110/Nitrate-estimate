48fe7b7b635ff42195850e6cb6944a59bf80c80c
ikyrie1110 <126843054+ikyrie1110@users.noreply.github.com>
2025-07-17 10:14:21 +0800

    Update data_preprocessing.py

--- data_processing_and_plotting/data_preprocessing.py
+++ data_processing_and_plotting/data_preprocessing.py
@@ -5,9 +5,11 @@ def load_data(data_path):
     return data
 
 def preprocess_data(data):
-    data = data.drop(columns=['station'])
+    data = data.drop(columns=['station'], errors='ignore')
     data = data.apply(pd.to_numeric, errors='coerce')
-    data = data.fillna(data.mean())  
+    data = data.fillna(data.mean())
+
+    # 时间字段处理
     data['Year'] = data['DATE'].astype(str).str[:4].astype(int)
     data['Month'] = data['DATE'].astype(str).str[-2:].astype(int)
     data['Quarter'] = (data['Month'] - 1) // 3 + 1
