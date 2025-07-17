02939ee0b9bba583a9b1b8db443ba256bc758183
ikyrie1110 <126843054+ikyrie1110@users.noreply.github.com>
2025-07-17 10:16:55 +0800

    Update stacking.py

--- data_processing_and_plotting/stacking.py
+++ data_processing_and_plotting/stacking.py
@@ -1,26 +1,24 @@
 from sklearn.model_selection import KFold
 import numpy as np
+import pandas as pd
 from sklearn.metrics import mean_squared_error, r2_score
 
 def generate_stacking_features(X, y, models, n_splits=5):
     kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
-    stacking_features = np.zeros((X.shape[0], len(models)))
-
-    for fold, (train_idx, val_idx) in enumerate(kf.split(X)):
-        X_train_fold, X_val_fold = X.iloc[train_idx], X.iloc[val_idx]
-        y_train_fold = y.iloc[train_idx]
-
-        for model_idx, (name, model) in enumerate(models.items()):
+    meta_features = pd.DataFrame(index=X.index)
+    for name, model in models.items():
+        preds = np.zeros(len(X))
+        for train_idx, val_idx in kf.split(X):
+            X_train_fold, X_val_fold = X.iloc[train_idx], X.iloc[val_idx]
+            y_train_fold = y.iloc[train_idx]
             model.fit(X_train_fold, y_train_fold)
-            stacking_features[val_idx, model_idx] = model.predict(X_val_fold)
+            preds[val_idx] = model.predict(X_val_fold)
+        meta_features[name] = preds
+    return meta_features
 
-    return stacking_features
-
-def stack_and_evaluate(X_train_stacking, y_train, X_test_stacking, y_test, meta_model):
-    meta_model.fit(X_train_stacking, y_train)
-    y_pred = meta_model.predict(X_test_stacking)
-
-    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+def stack_and_evaluate(X_train_stack, y_train, X_test_stack, y_test, meta_model):
+    meta_model.fit(X_train_stack, y_train)
+    y_pred = meta_model.predict(X_test_stack)
+    rmse = mean_squared_error(y_test, y_pred, squared=False)
     r2 = r2_score(y_test, y_pred)
-
     return rmse, r2
